7515942be3dd640651d23a46620b152c57d27787
chotj <chotjdwns654@naver.com>
2023-01-03 23:36:47 +0900

    Geometry종류

---
+++ sj/02_geometry_circle.js
@@ -0,0 +1,158 @@
+import * as THREE from '../build/three.module.js';
+// 마우스를 이용해 Object를 회전시키기 위한 코드
+import { OrbitControls } from "../examples/jsm/controls/OrbitControls.js"
+
+// three.js의 구성
+
+// 1. Renderer : Scene을 모니터에 렌더링(출력)할 수 있는 장치
+
+// 1-1. Scene : 3차원 객체로 구성되는 장면
+
+// 1-1-1. Light : 3차원 형상을 화면에 표시하기위한 광원
+// 1-1-2. Mesh (Object3D) : Object3D의 파생 클래스
+
+// 1-1-2-1. Geometry : 형상을 정의
+// 1-1-2-2. Material : 색상 및 투명도 정의
+
+// 1-2. Camera : Scene을 어떤 지점에서 볼지를 정하는 장치
+
+class App{
+    // 약속
+    // 1. "_"이 앞에 붙은 메서드는 이 App클래스 내부에서만 사용되는 private라는 뜻
+    constructor() {
+        // html에서 3D를 띄워줄 div 선언하기
+        const divContainer = document.querySelector("#webgl-container");
+        // 다른 메서드에서 참조할 수 있도록 field로 정의
+        this._divContainer = divContainer;
+
+        // Renderer 생성 (antialias : 렌더링 시 물체의 경계선을 부드럽게 표시)
+        const renderer = new THREE.WebGLRenderer({antialias:true});
+        // pixel의 ratio값 설정
+        renderer.setPixelRatio(window.devicePixelRatio);
+        // renderer의 domElement를 id가 webgl-container인 div의 자식으로 추가
+        divContainer.appendChild(renderer.domElement);
+        // 다른 메서드에서 참조할 수 있도록 field로 정의
+        this._renderer = renderer; // 이 renderer는 canvas타입의 dom 객체
+
+        // scene객체 생성
+        const scene = new THREE.Scene();
+        // 다른 메서드에서 참조할 수 있도록 field로 정의
+        this._scene = scene;
+
+        // 메서드 호출 (아직 정의 X)
+        this._setupCamera();
+        this._setupLight();
+        this._setupModel();
+        this._setupControls();
+
+        // window.onresize : 창 크기 변경시 발동되는 장치
+        // renderer와 camera는 창 크기가 변경될 때마다 그 크기에 맞게 속성값을 재설정 해줘야 하기 때문
+        // bind : resize method안에서 this가 가르키는 객체가 이벤트 객체가 아닌 이 App
+        //          클래스의 객체가 되도록하기 위해 사용
+        window.onresize = this.resize.bind(this);
+        this.resize();
+
+        // render : 3차원 그래픽 장면을 만들어주는 메서드
+        // render메서드를 requestAnimationFrame에 넘겨줌으로써 render메서드 호출
+        // bind를 사용한 이유 : render메서드의 코드안에서 사용되는 this가 바로 이
+        //                      app클래스의 객체를 가르키기 위해
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    // 위에서 정의하지 않고 호출만 한 메서드 생성
+    _setupControls(){
+        // OrbitControls : 마우스로 화면을 컨트롤하는 기능
+        // OrbitControls객체 = 카메라 객체 + 마우스 이벤트를 받는 Dom요소
+        new OrbitControls(this._camera, this._divContainer);
+    }
+    // 카메라
+    _setupCamera(){
+        // three.js가 3차원 그래픽을 출력할 영역의 가로, 세로 크기를 가져오기
+        const width = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+        // 카메라 객체 생성
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            width / height,
+            0.1,
+            100
+        );
+        camera.position.z = 2;
+        this._camera = camera;
+    }
+    // 빛
+    _setupLight(){
+        const color = 0xffffff;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color,intensity);
+        light.position.set(-1, 2, 4);
+        this._scene.add(light);
+    }
+    // 파란색 정육면체 mesh 생성
+    _setupModel(){
+        // BoxGeometry 정육면체의 형상
+        const geometry = new THREE.﻿TorusKnotGeometry(0.6,0.1,64,10,3,4);
+        // 파란색 계열의 재질 생성
+        const Material = new THREE.MeshPhongMaterial({color: 0x44a88});
+        // 회색 계열의 재질 생성
+        const fillMaterial = new THREE.MeshPhongMaterial({color:0x515151});
+
+        // 정육면체 생성
+        const cube = new THREE.Mesh(geometry,fillMaterial);
+
+        // 노란색 라인 생성
+        const lineMaterial = new THREE.LineBasicMaterial({color: 0xffff00});
+        const line = new THREE.LineSegments(
+            // WireframeGeometry : 모델의 외각선 표시
+            new THREE.WireframeGeometry(geometry),lineMaterial);
+
+        // 라인과 정육면체를 합칠 수 있도록 group 객체생성
+        const group = new THREE.Group();
+        // 그룹안에 정육면체와 라인 넣기
+        group.add(cube);
+        group.add(line);
+
+        // scene객체의 구성요소로 cube추가
+        this._scene.add(group)
+        // 다른 메서드에서 참조할 수 있도록 field로 정의
+        this._cube = group;
+    }
+
+    // 창의 크기가 변경될때 발생하는 이벤트
+    resize(){
+        // 위에서 divContainer로 정의한(#webgl-container div) div의 크기 얻어오기
+        const width = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+
+        this._camera.aspect = width / height;
+        this._camera.updateProjectionMatrix();
+
+        this._renderer.setSize(width,height);
+    }
+    
+    // time : 렌더링이 처음 시작된 이후 경과된 시간(ms 단위)
+    // time은 requestAnimationFrame 함수가 render함수에 전달해준 값이다
+    render(time){
+        // 랜더링 시에 scene을 카메라의 시점으로 렌더링하도록 만드는 장치
+        this._renderer.render(this._scene, this._camera);
+        // 속성값을 변경시켜 애니메이션 효과를 만드는 장치
+        this.update(time);
+        requestAnimationFrame(this.render.bind(this));
+    }
+    // render에서 전달받은 time을 사용하여 애니메이션 효과를 만드는 장치
+    update(time){
+        time *= 0.001;  // 알아보기 쉽게 ms단위를 초단위로 변경
+        // // 정육면체 자동 회전 장치
+        // this._cube.rotation.x = time;
+        // this._cube.rotation.y = time;
+    }
+
+}
+
+window.onload = function(){
+    new App();
+}
+
+
+
+
