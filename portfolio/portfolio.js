9ac486d0e018572558aeb8b93f51cc9c69b0e34a
SUNGJUN1234 <chotjdwns654@naver.com>
2023-05-13 17:03:24 +0900

    스크롤 줌이동 기능

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,13 +1,17 @@
 import * as THREE from '../build/three.module.js';
-import { OrbitControls } from "../examples/jsm/controls/OrbitControls.js"
 import {FBXLoader} from "../examples/jsm/loaders/FBXLoader.js"
 
-
+let scene_num = 0;
+let first_camera_position;
+let move_camera_position;
+let wheelEnabled = true;
 class App{
   
     constructor() {
-      
-        const divContainer = document.querySelector("#webgl-container");
+  
+
+
+        const divContainer = document.querySelector("#webgl_container");
         this._divContainer = divContainer;
 
         const renderer = new THREE.WebGLRenderer({antialias:true});
@@ -17,13 +21,13 @@ class App{
         this._renderer = renderer;
 
         const scene = new THREE.Scene();
+
         this._scene = scene;
         
         this._setupCamera();
-        this._setupAudio();
         this._setupLight();
         this._setupModel();
-        this._setupControls();
+        this._setupEventListeners();
         this._clock = new THREE.Clock();
 
         window.onresize = this.resize.bind(this);
@@ -32,24 +36,74 @@ class App{
         requestAnimationFrame(this.render.bind(this));
 
     }
+    _setupEventListeners() {
+        // 마우스 스크롤 이벤트 리스너 추가
+        window.addEventListener('wheel', this._handleMouseScroll.bind(this));
+    }
+    _handleMouseScroll(event) {
+        if (!wheelEnabled) {
+            return;
+          }
+          wheelEnabled = false;
+          setTimeout(() => {
+            wheelEnabled = true;
+          }, 1800);
 
-    _setupAudio() {
-        const listener = new THREE.AudioListener();
-        this._camera.add(listener); // 카메라에 추가합니다.
-    
-        const audioLoader = new THREE.AudioLoader();
-        audioLoader.load('data/beat.mp3', (buffer) => {
-          const audio = new THREE.Audio(listener);
-          audio.setBuffer(buffer);
-          audio.setLoop(true); // 반복 재생 설정
-          audio.setVolume(0.5); // 볼륨 설정
-          audio.play(); // 재생 시작
-        });
+        const deltaY = event.deltaY; // 스크롤 이동 값
+
+        if (deltaY > 0) {
+          if (scene_num < 4) {
+            ++scene_num;
+          }
+        } else {
+          if (scene_num > 0) {
+            --scene_num;
+          }
+        }
+      
+        console.log(scene_num)
+      
+        if (scene_num == 0) {
+          move_camera_position = { x: 0, y: 88, z: 170 };
+        } else if (scene_num == 1) {
+          move_camera_position = { x: 0, y: 150, z: 50 };
+        }else if (scene_num == 2) {
+            move_camera_position = { x: 0, y: 10, z: 50 };
+        }else if (scene_num == 3) {
+            move_camera_position = { x: 0, y: 88, z: 800 };
+        }else if (scene_num == 4) {
+            move_camera_position = { x: 0, y: 70, z: 50 };
+        }
+      
+        let duration = 1700; // 애니메이션 지속 시간 (밀리초)
+        const startTime = performance.now(); // 시작 시간
+
+        // 애니메이션 프레임 함수
+        const animateCamera = (currentTime) => {
+          const elapsedTime = currentTime - startTime; // 경과 시간
+          let progress = Math.min(elapsedTime / duration, 1); // 진행률 (0~1)
+      
+          // 현재 위치에서 목표 위치까지 일정한 간격으로 이동
+          this._camera.position.copy(
+            first_camera_position.clone().lerp(move_camera_position, progress/10)
+          );
+      
+          // 애니메이션 종료 조건
+          if (progress < 1) {
+            requestAnimationFrame(animateCamera);
+          } else {
+            // 애니메이션이 끝나면 first_camera_position을 갱신
+            first_camera_position.copy(move_camera_position);
+          }
+        };
+      
+        // 애니메이션 시작
+        requestAnimationFrame(animateCamera);
+      
+        // 카메라의 바라보는 방향을 설정하거나 변경해야 한다면 아래 코드 사용
+        // this._camera.lookAt(targetX, targetY, targetZ);
       }
 
-      _setupControls(){
-        this._controls = new OrbitControls(this._camera, this._renderer.domElement);
-    }
     _setupCamera(){
         const width = this._divContainer.clientWidth;
         const height = this._divContainer.clientHeight;
@@ -117,37 +171,36 @@ class App{
         camera.lookAt(centerBox.x, centerBox.y, centerBox.z);
 
         // 카메라의 위치를 발 끝이 아닌 몸의 중심으로 할 수 있도록 만드는 코드
-        this._controls.target.set(centerBox.x, centerBox.y, centerBox.z);
-    
+
+        first_camera_position = this._camera.position;
+            console.log(first_camera_position)
     }
 
 
     _setupModel(){
-       const loader = new FBXLoader();
-       loader.load('data/dancing.fbx', object => {
-        object.traverse(node => {
-          if (node.isMesh) {
-            node.geometry.computeBoundingBox(); // 메시의 크기에 맞게 Box3 객체의 크기 조정
-          }
+        const loader = new FBXLoader();
+        loader.load('data/dancing.fbx', object => {
+            object.traverse(node => {
+                if (node.isMesh) {
+                    node.geometry.computeBoundingBox(); // 메시의 크기에 맞게 Box3 객체의 크기 조정
+                }
+            });
+    
+            this._mixer = new THREE.AnimationMixer(object);
+            const action = this._mixer.clipAction(object.animations[0]);
+            action.play();
+    
+            this._scene.add(object);
+    
+            this._zoomFit(object, this._camera, "Z", true);
+    
+            this._clock = new THREE.Clock();
+    
+            this._cleanUp = () => {
+                this._mixer.stopAllAction();
+                this._mixer.uncacheRoot(object);
+            };
         });
-      
-        this._mixer = new THREE.AnimationMixer(object);
-        const action = this._mixer.clipAction(object.animations[0]);
-        action.play();
-      
-        this._scene.add(object);
-              
-        this._zoomFit(object, this._camera, "Z", true);
-      
-        this._clock = new THREE.Clock();
-      
-        this._cleanUp = () => {
-          this._mixer.stopAllAction();
-          this._mixer.uncacheRoot(object);
-        };
-      });
-
-  
     }
 
     resize(){
---
+++ portfolio/sub.js
@@ -0,0 +1,19 @@
+// 오디오 요소 생성
+var audio = new Audio('./data/beat.mp3');
+audio.play();
+
+// 재생 버튼 클릭 시
+document.getElementById('play_button').addEventListener('click', function() {
+    audio.play();
+});
+
+// 일시 정지 버튼 클릭 시
+document.getElementById('pause_button').addEventListener('click', function() {
+    audio.pause();
+});
+
+// 정지 버튼 클릭 시
+document.getElementById('stop_button').addEventListener('click', function() {
+    audio.pause();
+    audio.currentTime = 0;
+});
